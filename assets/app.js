966bc027988385e486ed68a46ec622c1d2e0f4e6
Wills Grave <will@keydigitalconsulting.com>
2019-09-18 23:46:11 -0400

    Add the apiKey into the var config and intializeapp as well as var database.

---
+++ assets/app.js
@@ -0,0 +1,14 @@
+// 1. Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBmIAtoYfOPXXZ8uAdUE5zjaMSq5Z_MpOA",
+  authDomain: "fir-eba9f.firebaseapp.com",
+  databaseURL: "https://fir-eba9f.firebaseio.com",
+  projectId: "fir-eba9f",
+  storageBucket: "fir-eba9f.appspot.com",
+  messagingSenderId: "916143939246",
+  appId: "1:916143939246:web:77d3d23b0c06ed1ae7173f"
+};
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
